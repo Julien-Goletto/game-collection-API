e7ab7e2629788fea232274901733fa495f1dacb2
Julien GOLETTO <julien.goletto@gmail.com>
2022-04-16 20:28:06 +0200

    Progrès sur les écritures automatiques des queries SQL, besoin d'échapper les doubles quotes pour tous les strings passés en db (avec des Regex)

--- API/src/external_api/RAWG.service.js
+++ API/src/external_api/RAWG.service.js
@@ -2,48 +2,62 @@ require('dotenv').config({ path: '../../.env' });
 const axios = require('axios');
 const {RAWG_API_KEY} = process.env;
 
-const dataToExtract = ['name', 'platforms', 'released', 'background_image', 'genres'];
-
 const RAWG = {
+
+  dataToExtract: ['name', 'platforms', 'released', 'background_image', 'genres'],
+
   /**
    * Get platform list to recover basic informations
    * @returns {Array} game platform objects
    */
-  async GetPlatforms(RAWG_API_KEY){
-    const response = await axios(`https://api.rawg.io/api/platforms?key=${RAWG_API_KEY}`);
-    console.log(response.data.results);
+  async getPlatforms(RAWG_API_KEY){
+    const platformsList = await axios(`https://api.rawg.io/api/platforms?key=${RAWG_API_KEY}`)
+    return platformsList;
   },
-  
+
   /**
-   * Get games list from a platform
+   * Get most pertinent game for a specific search, formated to be used later (the first of the returned list)
    * @param {string} RAWG_API_KEY - API key from .env variables
    * @param {integer} platformId
    * @param {string} searchQuery
-   * @returns {Array} game objects
+   * @returns {Object} the most pertinent object game
    */
-  async GetGamesFromPlatformIdAndSearchQuery (RAWG_API_KEY, platformId, searchQuery){
-    const response = await axios(`https://api.rawg.io/api/games?platforms=${platformId}&search=${searchQuery}&search_exact=true&key=${RAWG_API_KEY}`);
-    const results = response.data.results;
+  async getGamesFromPlatformIdAndSearchQuery (RAWG_API_KEY, platformId, searchQuery){
+    const response = await axios(`https://api.rawg.io/api/games?platforms=${platformId}&search=${searchQuery}&search_precise=true&key=${RAWG_API_KEY}`);
+    const formatedGamesArray = RAWG.formatGamesData(response.data.results[0]);
+    return formatedGamesArray;
+  },
 
-    // Formating results
-    let games = [];
-    for (let result of results){
-      const gameObject = Object.fromEntries(Object.entries(result).filter(([key,_]) => dataToExtract.includes(key)));
-      // Formating data specificly for platforms and genres  
-      reformatedPlatforms = [];
-      reformatedGenres = [];
-      for (let platform of gameObject.platforms){
-        reformatedPlatforms.push(platform.platform.name);
-      }
-      for (let genre of gameObject.genres){
-        reformatedGenres.push(genre.name);
-      }
-      gameObject.platforms = reformatedPlatforms;
-      gameObject.genres = reformatedGenres;
-      games.push(gameObject);
+  /**
+   * Format a game object returned by RAWG API
+   * @param {Object} gameFromRAWGAPI Game returned, to format
+   * @returns {Object} A formated game object
+   */
+  formatGamesData(gameFromRAWGAPI){
+    const gameObject = Object.fromEntries(Object.entries(gameFromRAWGAPI).filter(([key,_]) => RAWG.dataToExtract.includes(key)));
+    // Formating data specificly for platforms and genres  
+    reformatedPlatforms = [];
+    reformatedGenres = [];
+    for (let platform of gameObject.platforms){
+      reformatedPlatforms.push(platform.platform.name);
     }
-    console.log(games);
-    return games;
+    for (let genre of gameObject.genres){
+      reformatedGenres.push(genre.name);
+    }
+    gameObject.platforms = reformatedPlatforms;
+    gameObject.genres = reformatedGenres;
+    return gameObject;
+  },
+
+
+  async createGamesPayload (RAWG_API_KEY, gamesToAdd){
+    const gamesPayloadPromises = [];
+    for (game of gamesToAdd){
+      const results = RAWG.getGamesFromPlatformIdAndSearchQuery (RAWG_API_KEY, game.platformId,game.gameTitle);
+      gamesPayloadPromises.push(results);
+    };
+    const gamesPayload = await Promise.all(gamesPayloadPromises); //Considérer allSettled pour une gestion des erreurs
+    return gamesPayload;
   },
 };
 
