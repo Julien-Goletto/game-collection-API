7b6711fd274189dd9e85fcd802cd8c654e667d76
Julien GOLETTO <julien.goletto@gmail.com>
2022-04-25 16:28:03 +0200

    Implémentation du hashage des mots de passe ok

--- API/src/database/models/users.datamapper.js
+++ API/src/database/models/users.datamapper.js
@@ -2,18 +2,23 @@ const client = require('../dbclient');
 const debug = require('debug')("User_DataMapper");
 const APIError = require('../../Errors/APIError');
 
+const bcrypt = require('bcryptjs');
+
 const usersDataMapper = {
   /**
-   * Save a  new user in database
+   * Save a  new user in database, using bcrypt for password encrypting
    * @param {Object} user 
    * @returns {String} feedback message
    * @throws {APIError} if user already in base, due to unique constraint
    */
   async createNewUser(user) {
+    let {pseudo, password} = user;
+    const salt = await bcrypt.genSalt(10);
+    const encryptedPassword = await bcrypt.hash(password,salt);
     const query = {
       text : `INSERT INTO "user"("pseudo","password") VALUES ($1,$2)`,
-      values:[user.pseudo,user.password],
-    }
+      values:[pseudo,encryptedPassword],
+    };
     const results = await client.query(query);
     if(!results.rowCount){
       throw new APIError ("This pseudo is already taken. Please choose another one.", 404);
@@ -21,7 +26,7 @@ const usersDataMapper = {
     return 'User successfully registered, please login to continue.';
   },
   /**
-   * From a user object, return a matching user
+   * From a user object, return a matching user, comparing hash in DB to entered password
    * @param {Object} user 
    * @returns {Object} user informations from db to be processed in controller
    * @throws {APIError} if credentials doesn't match
@@ -29,13 +34,17 @@ const usersDataMapper = {
   async getUser(user) {
     const query = {
       text : `SELECT pseudo, is_admin, password FROM "user"
-              WHERE pseudo = $1 AND password = $2`,
-      values:[user.pseudo,user.password],
+              WHERE pseudo = $1`,
+      values:[user.pseudo],
     }
     const results = await client.query(query);
     if(!results.rowCount){
-      throw new APIError ("Credentials don't match, please retry.", 404);
+      throw new APIError ("This account doesn't exist.", 404);
     };
+    const isCorrect = await bcrypt.compare(user.password,results.rows[0].password);
+    if(!isCorrect){
+      throw new APIError("Credentials don't match, please retry.",404);
+    }
     return results.rows[0];
   },
   /**
